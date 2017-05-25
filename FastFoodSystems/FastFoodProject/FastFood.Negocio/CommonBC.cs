3071ff1d02c24b51543d6d6cefd7861a3fc9104b
Jesús Pérez <jesus-perez95@hotmail.com>
2017-05-25 11:37:17 -0300

    [add] Proyecto base funcionando la conexion

---
+++ FastFoodSystems/FastFoodProject/FastFood.Negocio/CommonBC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FastFood.DALC;
+
+
+namespace FastFood.Negocio
+{
+    class CommonBC
+    {
+        private static FastFoodEntities _modeloFastFood;
+        public static FastFoodEntities ModeloFastFood
+        {
+            get
+            {
+                if (_modeloFastFood == null)
+                {
+                    _modeloFastFood = new FastFoodEntities();
+                }
+                return _modeloFastFood;
+            }
+        }
+
+        public CommonBC() { }
+    }
+}
+
---
+++ FastFoodSystems/FastFoodProject/FastFoodProject/CajeroPaginas/SistemaProductos.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FastFoodProject
+{
+    /// <summary>
+    /// Lógica de interacción para Sistema_Productos.xaml
+    /// </summary>
+    public partial class Sistema_Productos : Page
+    {
+        FastFood.Negocio.ProductoCollection productos = new FastFood.Negocio.ProductoCollection();
+        public Sistema_Productos()
+        {
+            InitializeComponent();
+            CollectionViewSource itemCollectionViewSource;
+            itemCollectionViewSource = (CollectionViewSource)(FindResource("productoViewSource"));
+            itemCollectionViewSource.Source = productos.GetProductos();
+        }
+
+        private void dataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void productoDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Console.WriteLine(productoDataGrid.SelectedItem);
+        }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Console.WriteLine(productoDataGrid.SelectedItem);
+        }
+
+    }
+}

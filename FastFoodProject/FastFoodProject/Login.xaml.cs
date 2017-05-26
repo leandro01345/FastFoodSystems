cb79fb68b7889a0436665d68843f1be4c8c7e62c
leandro <hideki_hinata-@hotmail.com>
2017-05-25 22:42:16 -0400

    Login[Add Diseño]
    Login

--- FastFoodProject/FastFoodProject/Login.xaml.cs
+++ FastFoodProject/FastFoodProject/Login.xaml.cs
@@ -23,5 +23,10 @@ namespace FastFoodProject
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
